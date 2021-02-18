35ef07ce52f75ab7f9e1a25924529b8dc91ca841
Hally <hbianan@gmail.com>
2021-02-18 18:13:28 -0500

    fixing deployment issues

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://upbeat-swanson-0f463c.netlify.app/'
+export const server = dev ? 'http://localhost:3000' : 'https://upbeat-swanson-0f463c.netlify.app'
